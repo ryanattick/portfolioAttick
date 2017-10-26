a600198ae683c6906868a6e4cdd14e3b82b0da46
Ryan <your_email@whatever.comclear>
2017-10-26 12:33:18 -0700

    adds projects

--- src/Home.jsx
+++ src/Home.jsx
@@ -18,11 +18,14 @@ class Home extends Component {
           >
             <strong> RYAN SPENCER ATTICK </strong>
           </Subheader>
-
-            {/* I am a fullstack software engineer based in the San Francisco Bay Area looking for a challenging position with a company that is focused on making a positive social impact. */}
+          <div
+            style={{fontSize: '30px', width: '700px', height: '200px', float: 'right', marginRight: '100px', marginTop: '100px', color: 'rgba(0, 0, 0, 0.54)'}}
+          >
+            <strong>I am a fullstack software engineer based in the San Francisco Bay Area looking for a challenging position with a company that is focused on making a positive social impact.</strong>
+          </div>
         <img
           src="https://i.imgur.com/qYkliRF.jpg"
-          style={{marginTop: '80px', marginLeft: '20px', maxHeight: '300px', borderRadius: '150px'}}
+          style={{marginTop: '80px', marginLeft: '150px', maxHeight: '300px', borderRadius: '150px'}}
         />
       </div>
     );
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,18 +1,60 @@
 import React, { Component } from 'react';
+import {GridList, GridTile} from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 import './App.css';
 
-class Projects extends Component {
-  constructor(props) {
-    super(props);
+const styles = {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+  gridList: {
+    width: 1500,
+    height: 500,
+    overflowY: 'auto',
+  },
+};
 
+
+const tilesData = [
+  {
+    img: 'https://i.imgur.com/bS1EpQi.png',
+    title: 'Forgotten'
+    },
+  {
+    img: 'https://i.imgur.com/zXq7HOU.png',
+    title: 'Picognito'
+  },
+  {
+    img: 'https://i.imgur.com/9HiKW11.png',
+    title: 'PassPass'
   }
+];
 
-
+class Projects extends Component {
 
   render() {
     return (
-      <div>
-        PROJECTS
+      <div style={styles.root}>
+        <GridList
+          cols={3}
+          cellHeight={500}
+          style={styles.gridList}
+          padding={4}
+        >
+          {tilesData.map((tile) => (
+            <GridTile
+              key={tile.img}
+              title={tile.title}
+              // style={{marginLeft: '50px'}}
+
+            >
+              <img src={tile.img} />
+            </GridTile>
+          ))}
+        </GridList>
       </div>
     );
   }
