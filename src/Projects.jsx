7e9f7ef112335fbfcfffd0b7765a6a5ccf3cb5c4
Ryan <your_email@whatever.comclear>
2017-10-27 16:11:01 -0700

    fills in dialog boxes, adds link for PassPass

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -32,7 +32,8 @@ const tilesData = [
   },
   {
     img: 'https://i.imgur.com/9HiKW11.png',
-    title: 'PassPass'
+    title: 'PassPass',
+    link: 'https://youtu.be/vEC9heTK5GE'
   }
 ];
 
@@ -112,7 +113,7 @@ goToLink = (tile) => {
                   style={{height: '500px'}}
                 >
                   <img src='https://i.imgur.com/akXYYAY.png' style={{maxHeight: '250px'}}/> <br/><br/>
-                  Forgotten is a logic-based game. It's written in a text style and features a storyline in which the user must play through the levels to unlock items and secrets about their character. This web app was developed by a team of engineers working collaboratively over the span of a month. My role focused mainly on the user experience and interface.
+                  Forgotten is a logic-based game. It's written in a text style and features a storyline in which the user must play through the levels to unlock items and secrets about their character. This web app was developed by a team of four engineers working collaboratively over the span of a month. My role focused mainly on the user experience and interface.
                 </Dialog>
           <RaisedButton label="About Picognito" onClick={this.handleOpenPicognito} style={{margin: 'auto'}}/>
                 <Dialog
@@ -124,7 +125,7 @@ goToLink = (tile) => {
                   contentStyle={{textAlign: 'center'}}
                 >
                   <img src='https://i.imgur.com/Cg8oxk8.png'style={{maxHeight: '250px'}}/> <br/><br/>
-                  PICOGNITO
+                  Picognito is an anonymous image sharing platform based on geolocation technology. The app allows users to upload photos which are tagged by location automatically. Users can only see photos of other users that were tagged within a five mile radius of their physical location. Additionally, users can like, dislike, or comment on pictures. This app was developed for the web by a team of four engineers in the span of two weeks. My role involved developing the user interface as well as querying the database.
                 </Dialog>
           <RaisedButton label="About PassPass" onClick={this.handleOpenPassPass} style={{marginRight: '120px'}}/>
                 <Dialog
@@ -136,7 +137,7 @@ goToLink = (tile) => {
                   contentStyle={{textAlign: 'center'}}
                 >
                   <img src='https://i.imgur.com/400HqKP.png'style={{maxHeight: '250px'}}/> <br/><br/>
-                  PASSPASS
+                  PassPass is an web application focused on a user's ability to buy or sell ClassPasses. ClassPass is an existing service that allows consumers to buy entrance into individual gym classes without having to pay for a gym membership. These passes must be used within a certain period of time and cannot be used at the same gym more than once per cycle. Given these limitations, consumers might not be able to use all the passes before they expire. PassPass was created as a third-partying vendor to meet the need of users who might only want one or two ClassPasses for a lower rate than they would see from ClassPass, or for those who find themselves unable to use all the ClassPasses they originally purchased. This app was developed by a team of engineers in two weeks and then was passed on to my team. My team of four spent two additional weeks making improvements to the functionality and design of the application. My role focused on developing a more intuitive user interface as well as creating and refining database queries.
                 </Dialog>
           </div>
       </div>
