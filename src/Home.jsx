6031ac6c6472965a666d29eeb5e555927589c1da
Ryan <your_email@whatever.comclear>
2017-10-27 17:40:42 -0700

    adds flexbox to homepage

--- src/Home.jsx
+++ src/Home.jsx
@@ -9,24 +9,25 @@ class Home extends Component {
   }
 
 
-
   render() {
     return (
       <div>
         <Subheader
-          style={{textAlign: 'center', fontSize: '90px', marginTop: '50px'}}
+          style={{textAlign: 'center', fontSize: '7vw', marginTop: '50px'}}
           >
             <strong> RYAN SPENCER ATTICK </strong>
           </Subheader>
-          <div
-            style={{fontSize: '25px', width: '700px', height: '200px', float: 'right', marginRight: '100px', marginTop: '100px', color: 'rgba(0, 0, 0, 0.54)'}}
-          >
-            <strong>I am a fullstack software engineer based in the San Francisco Bay Area specializing in frontend frameworks. My focus has primarily been on building efficient Javascript applications that create beautiful user interfaces and intuitive user flow. I'm passionate about using new and innovative concepts to solve problems and build comprehensive applications.</strong>
+          <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'nowrap', justifyContent: 'center', alignContent: 'stretch', alignItems: 'center'}}>
+            <img
+              src="https://i.imgur.com/qYkliRF.jpg"
+              style={{maxHeight: '300px', borderRadius: '150px', margin: '70px'}}
+            />
+            <div
+              style={{maxWidth: '600px', height: '200px', fontSize: '2vw', color: 'rgba(0, 0, 0, 0.54)'}}
+            >
+              <strong>I am a fullstack software engineer based in the San Francisco Bay Area specializing in frontend frameworks. My focus has primarily been on building efficient Javascript applications that create beautiful user interfaces and intuitive user flow. I'm passionate about using new and innovative concepts to solve problems and build comprehensive applications.</strong>
+            </div>
           </div>
-        <img
-          src="https://i.imgur.com/qYkliRF.jpg"
-          style={{marginTop: '80px', marginLeft: '150px', maxHeight: '300px', borderRadius: '150px'}}
-        />
       </div>
     );
   }
