e1428cf779a117f56e8ac79b5ec7ffef3757a1fe
Ryan <your_email@whatever.comclear>
2017-10-27 20:06:28 -0700

    fixes spacing in home

--- src/Home.jsx
+++ src/Home.jsx
@@ -23,7 +23,7 @@ class Home extends Component {
               style={{width: '20%', height: 'auto', borderRadius: '150px', margin: '70px'}}
             />
             <div
-              style={{maxWidth: '700px', height: '200px', fontSize: '2vw', color: 'rgba(0, 0, 0, 0.54)'}}
+              style={{maxWidth: '700px', height: '200px', fontSize: '2vw', color: 'rgba(0, 0, 0, 0.54)', lineHeight: '150%'}}
             >
               <strong>I am a fullstack software engineer based in the San Francisco Bay Area specializing in frontend frameworks. My focus has primarily been on building efficient Javascript applications that create beautiful user interfaces and intuitive user flow. I'm passionate about using new and innovative concepts to solve problems and build comprehensive applications.</strong>
             </div>
