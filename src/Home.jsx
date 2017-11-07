75b005b2296bb58b39cb79d839ca81ef166c0831
Ryan <your_email@whatever.comclear>
2017-11-06 18:39:47 -0800

    updates photo

--- src/Home.jsx
+++ src/Home.jsx
@@ -15,15 +15,15 @@ class Home extends Component {
         <Subheader
           style={{textAlign: 'center', fontSize: '7vw', marginTop: '50px'}}
           >
-            <strong> RYAN SPENCER ATTICK </strong>
+            <strong> RYAN ATTICK </strong>
           </Subheader>
           <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'nowrap', justifyContent: 'center', alignContent: 'stretch', alignItems: 'center'}}>
             <img
-              src="https://i.imgur.com/qYkliRF.jpg"
+              src="https://i.imgur.com/GAXyvyp.jpg"
               style={{width: '20%', height: 'auto', borderRadius: '150px', margin: '70px'}}
             />
             <div
-              style={{maxWidth: '700px', height: '200px', fontSize: '2vw', color: 'rgba(0, 0, 0, 0.54)', lineHeight: '150%'}}
+              style={{maxWidth: '700px', height: '200px', fontSize: '2vw', color: 'rgba(0, 0, 0, 0.54)', lineHeight: '140%'}}
             >
               <strong>I am a fullstack software engineer based in the San Francisco Bay Area specializing in frontend frameworks. My focus has primarily been on building efficient Javascript applications that create beautiful user interfaces and intuitive user flow. I'm passionate about using new and innovative concepts to solve problems and build comprehensive applications.</strong>
             </div>
