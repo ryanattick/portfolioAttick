fa5ba8451bc7ce9bae55a403604265cd352b28ae
Ryan <your_email@whatever.comclear>
2018-05-14 09:50:00 -0700

    add some responsiveness and fix card flip bug

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Subheader from 'material-ui/Subheader';
-import './App.css';
+import style from './App.css';
 
 class Home extends Component {
   constructor(props) {
@@ -12,14 +12,19 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <Subheader
-          style={{textAlign: 'center', fontSize: '7vw', marginTop: '50px'}}
-          >
-            <strong> RYAN ATTICK </strong>
+        <div class="bg-video">
+              <video class="bg-video__content" autoplay muted loop>
+                <source src="../public/Aloha-Mundo/MP4/Aloha-Mundo.mp4" type="video/mp4"/>
+                <source src="../public/Aloha-Mundo/WEBM/Aloha-Mundo.webm" type="video/webm"/>
+                  Your browser is not supported :(
+              </video>
+        </div>
+        <Subheader class="subheader">
+            <strong> SPENCER ATTICK </strong>
           </Subheader>
           <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'nowrap', justifyContent: 'center', alignContent: 'stretch', alignItems: 'center'}}>
             <img
-              src="https://i.imgur.com/GAXyvyp.jpg"
+              src="https://lh3.googleusercontent.com/16p91QuOxLXjdZ-SID_grZIzmz6byfjPFDxhC8OWOuGH-V3GhE7PCZQQrZndhtS-hdSEXfMap3D6EmZ2P6spsQnyDEkiwmzHAEWu7Hco43QbkKjfmGTQ7iZgnv6rwDuzKjwqza1r2811K0pG5EleEeEaQ-N0q0nh_WKwM4WqMebqcabw0ILnkIGsE0001OjgPuQh1Uyj_pKM6qGO8WpQjxENSB0h0iGRAjQHuK_UtiIZ30GlvBNWctWpQ6XR0jPkcNnITHS5clQbglsGFtGoMbJMJ6VartlK4y5Vdy4bqMH-8gbuXzlGFUGYNM384dYT6XrcifzIYcJW85Jo3duNrLlSnfwWCenFKmo_i9n2IrgpNHnuBwoY4IdjZOkkZ4yH24uV3FhVUnd7d-IKxnwSUoUPeynzNXANcxlLS2G7T1sq9OQdi3tieSWaWnCgDU1yURNdRUERFu19qQOb7zL2wWWxhV5Ctf2fkmxbJTL9hCOY-G0mP-dT9X6muHVBfajK2QkkksAmYa6eEx93Sx3oMDdYvv2n31DaCBm_UQdhsJMYDI30Im-Kz8rBHFrq1E16MlHxpvj36jG9rIxPXiW64JRB1EdYcrxAZvNzUfc=s200-no"
               style={{width: '20%', height: 'auto', borderRadius: '150px', margin: '70px'}}
             />
             <div
